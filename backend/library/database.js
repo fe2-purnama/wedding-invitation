7924c49404f4a4f18448f5003e1031a240823dcd
mahmud-bsr <mahmudfbasyarahil88@gmail.com>
2024-06-17 22:55:18 +0700

    login register user

---
+++ backend/library/database.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+
+// Buat pool koneksi ke database MySQL
+const pool = mysql.createPool({
+  connectionLimit: 10, // jumlah maksimal koneksi dalam pool
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'wedding_db'
+});
+
+// Kondisi untuk mengecek koneksi database
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+  } else {
+    console.log('Connected to MySQL database successfully!');
+    connection.release(); // lepaskan koneksi setelah selesai
+  }
+});
+
+module.exports = pool;
