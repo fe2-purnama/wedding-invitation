fbffc9ea9148fd144271e8801a925692e1bb876a
Mr-Tirta <109189090+Mr-Tirta@users.noreply.github.com>
2024-06-18 11:48:16 +0700

    Revisi dan penambahan tabel

--- backend/wedding_db.sql
+++ backend/wedding_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 17, 2024 at 05:32 PM
+-- Generation Time: Jun 18, 2024 at 04:41 AM
 -- Server version: 10.11.7-MariaDB-log
 -- PHP Version: 8.1.10
 
@@ -25,7 +25,6 @@ SET time_zone = "+00:00";
 
 --
 -- Table structure for table `tbl_admins`
--- Table structure for table `tbl_admins`
 --
 
 CREATE TABLE `tbl_admins` (
@@ -35,17 +34,22 @@ CREATE TABLE `tbl_admins` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Dumping data for table `tbl_admins`
+--
+
+INSERT INTO `tbl_admins` (`id_admin`, `username`, `email`, `password`) VALUES
+(2, 'admin', 'Admin@gmail.com', '$2a$10$RO5a6UgZRaAGbnAul.VcJOxZa/U3uEMy7jICmpMIurMyO46F1KAyS');
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `tbl_detail_invitations`
+--
+
 CREATE TABLE `tbl_detail_invitations` (
   `id_undangan` int(11) NOT NULL,
   `nama` varchar(250) NOT NULL,
-  `bride_name` varchar(100) NOT NULL,
-  `groom_name` varchar(100) NOT NULL,
-  `wedding_date` date NOT NULL,
-  `location` varchar(255) NOT NULL
   `bride_name` varchar(100) NOT NULL,
   `groom_name` varchar(100) NOT NULL,
   `wedding_date` date NOT NULL,
@@ -56,6 +60,8 @@ CREATE TABLE `tbl_detail_invitations` (
 
 --
 -- Table structure for table `tbl_guests`
+--
+
 CREATE TABLE `tbl_guests` (
   `id_undangan` int(11) NOT NULL,
   `nama` varchar(250) NOT NULL,
@@ -66,6 +72,8 @@ CREATE TABLE `tbl_guests` (
 
 --
 -- Table structure for table `tbl_invitations`
+--
+
 CREATE TABLE `tbl_invitations` (
   `id_undangan` int(11) NOT NULL,
   `nama` varchar(250) NOT NULL
@@ -90,13 +98,21 @@ CREATE TABLE `tbl_rsvps` (
 --
 
 CREATE TABLE `tbl_users` (
-  `id_user` int(11) NOT NULL,
+  `id_users` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `phone` varchar(50) NOT NULL,
-  `address` varchar(100) NOT NULL
+  `address` varchar(100) NOT NULL,
+  `role` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`id_users`, `username`, `email`, `password`, `phone`, `address`, `role`) VALUES
+(1, 'mail', 'mail@gmail.com', '$2a$10$il3FCIFOyfvcTxlujwDmee2w5T0jjJhQ4C0v1IkoIBIJ5uqMCikwe', '012345678999', 'Surabaya', 'user');
 
 --
 -- Indexes for dumped tables
@@ -110,13 +126,14 @@ ALTER TABLE `tbl_admins`
 
 --
 -- Indexes for table `tbl_detail_invitations`
+--
 ALTER TABLE `tbl_detail_invitations`
   ADD PRIMARY KEY (`id_undangan`),
-  ADD UNIQUE KEY `user_id` (`nama`);
   ADD UNIQUE KEY `user_id` (`nama`);
 
 --
 -- Indexes for table `tbl_guests`
+--
 ALTER TABLE `tbl_guests`
   ADD PRIMARY KEY (`id_undangan`),
   ADD UNIQUE KEY `nama_tamu` (`nama`);
@@ -139,7 +156,7 @@ ALTER TABLE `tbl_rsvps`
 -- Indexes for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  ADD PRIMARY KEY (`id_user`);
+  ADD PRIMARY KEY (`id_users`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -149,7 +166,7 @@ ALTER TABLE `tbl_users`
 -- AUTO_INCREMENT for table `tbl_admins`
 --
 ALTER TABLE `tbl_admins`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_detail_invitations`
@@ -167,7 +184,7 @@ ALTER TABLE `tbl_guests`
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_users` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -182,18 +199,7 @@ ALTER TABLE `tbl_detail_invitations`
 
 --
 -- Constraints for table `tbl_guests`
--- Constraints for table `tbl_guests`
 --
-ALTER TABLE `tbl_guests`
-  ADD CONSTRAINT `tbl_guests_ibfk_1` FOREIGN KEY (`id_undangan`) REFERENCES `tbl_rsvps` (`id_undangan`),
-  ADD CONSTRAINT `tbl_guests_ibfk_2` FOREIGN KEY (`nama`) REFERENCES `tbl_rsvps` (`nama`);
-
---
--- Constraints for table `tbl_invitations`
---
-ALTER TABLE `tbl_invitations`
-  ADD CONSTRAINT `tbl_invitations_ibfk_1` FOREIGN KEY (`id_undangan`) REFERENCES `tbl_guests` (`id_undangan`),
-  ADD CONSTRAINT `tbl_invitations_ibfk_2` FOREIGN KEY (`nama`) REFERENCES `tbl_guests` (`nama`);
 ALTER TABLE `tbl_guests`
   ADD CONSTRAINT `tbl_guests_ibfk_1` FOREIGN KEY (`id_undangan`) REFERENCES `tbl_rsvps` (`id_undangan`),
   ADD CONSTRAINT `tbl_guests_ibfk_2` FOREIGN KEY (`nama`) REFERENCES `tbl_rsvps` (`nama`);
